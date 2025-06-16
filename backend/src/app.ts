0be3a1adf26a8f4503d71994ffd8bab6a8eb9b95
Sameer Basnet <basnetsameer78@gmail.com>
2025-06-16 19:35:38 +0545

    fix: update welcome message to reflect correct application name

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Welcome to the Blog Fullstack API",
+    message: "Welcome to the Bank Fullstack API",
   });
   return;
 });
